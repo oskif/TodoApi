ff73bc412dc740aaa9c1be4d92e3cf6a6232d2f7
oskif <oskar.fojcik123@gmail.com>
2025-04-28 10:19:03 +0200

    Create data model

---
+++ Todo.Api/Entities/TodoEntity.cs
@@ -0,0 +1,25 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Todo.Api.Entities;
+
+[Table("todo")]
+public class TodoEntity
+{
+    [Key]
+    [Column("id")]
+    public int Id { get; set; }
+
+    [Column("title")]
+    public required string Title { get; set; }
+
+    [Column("description")]
+    public required string Description { get; set; }
+
+    [Column("expiration_date_time")]
+    public required DateTime ExpirationDateTime { get; set; }
+
+    [Column("percent_of_complete")]
+    public decimal PercentOfComplete { get; set; }
+}
